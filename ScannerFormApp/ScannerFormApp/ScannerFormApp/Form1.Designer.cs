ba82e8fe534d4143420b5c78499337ac716ec764
Moamen Ahmed <moamen.ahmed1@hotmail.com>
2018-10-31 01:01:04 +0200

    whole project remaining testing and output file

---
+++ ScannerFormApp/ScannerFormApp/ScannerFormApp/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace ScannerFormApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input = new System.Windows.Forms.RichTextBox();
+            this.start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rb_input
+            // 
+            this.input.Location = new System.Drawing.Point(54, 21);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(238, 170);
+            this.input.TabIndex = 0;
+            this.input.Text = "";
+            // 
+            // b_Start
+            // 
+            this.start.Location = new System.Drawing.Point(140, 222);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.TabIndex = 1;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 322);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.input);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox input;
+        private System.Windows.Forms.Button start;
+    }
+}
+
---
+++ ScannerFormApp/ScannerFormApp/ScannerFormApp/Form1.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScannerFormApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public List<string> values=new List<string>();
+        public List<string> types=new List<string>();
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            string code = input.Text.ToString()+" ";//space added at last of code to simulate [other]
+            scanner2(code);
+        }
+
+        private bool isSymbol(char str)
+        {
+            if (str == '+' || str == '-' ||
+               str == '*' || str == '/' ||
+               str == '=' || str == '<' ||
+               str == '(' || str == ')' ||
+               str == ';' )
+                return true;
+            else return false;
+        }
+
+        private bool isReserved(string str)
+        {
+            if (str == "if" || str == "then" || str == "else" ||
+               str == "end" || str == "repeat" || str == "until" ||
+               str == "read" || str == "write")
+                return true;
+            else return false;
+        }
+
+        private void scanner(string code)
+        {
+
+            int state = 0;
+            string current_string="";
+            var builder = new StringBuilder();
+            for(int i=0; i<code.Length; i++)
+            {
+                if(char.IsLetter(code[i]) && state==0)
+                {
+                    builder.Append(code[i]);
+                    current_string = builder.ToString();
+                    state = 1;
+                    continue;
+                }
+                if (char.IsDigit(code[i]) && state == 0)
+                {
+                    state = 2;
+                    continue;
+                }
+                if (char.IsLetterOrDigit(code[i]) && state==1)
+                {
+                    builder.Append(code[i]);
+                    current_string = builder.ToString();
+                    continue;
+                }
+                if (!char.IsLetterOrDigit(code[i]))//symbol or space
+                {
+
+                    if(current_string!="")
+                    {
+                        values.Add(current_string);
+                        types.Add("identifier");
+                        current_string = "";
+                        state = 0;
+                    }
+                    
+                    
+                }
+            }
+        }
+        private void scanner2(string code)
+        {
+            int state = 0;
+            string current_string = "";
+            var builder = new StringBuilder();
+            int i = 0;
+            while (i < code.Length)
+            {
+                switch (state)
+                {
+                    case 0://start
+                        if(code[i]==' ' || code[i]=='\n')
+                        {
+                            state = 0;
+                            i++;
+                        }
+                        else if(code[i]=='{')
+                        {
+                            state = 1;
+                            i++;
+                        }
+                        else if(isSymbol(code[i]))
+                        {
+                            builder.Append(code[i]);
+                            current_string = builder.ToString();
+                            values.Add(current_string);
+                            types.Add("symbol");
+                            current_string = "";
+                            builder.Clear();
+                            state = 0;
+                            i++;
+                        }
+                        else if(char.IsDigit(code[i]))
+                        {
+                            builder.Append(code[i]);
+                            current_string = builder.ToString();
+                            state = 2;//number
+                            i++;
+                        }
+                        else if (char.IsLetter(code[i]))
+                        {
+                            builder.Append(code[i]);
+                            current_string = builder.ToString();
+                            state = 3;//id
+                            i++;
+                        }
+                        else if(code[i]==':')
+                        {
+                            builder.Append(code[i]);
+                            current_string = builder.ToString();
+                            state = 4;//assign
+                            i++;
+                        }
+                        break;
+
+                    case 1://comment
+                        if(code[i]!='}')
+                        {
+                            state = 1;//comment
+                            i++;
+                        }
+                        else
+                        {
+                            state = 0;//start
+                            i++;
+                        }
+                        break;
+                    case 2://number
+                        if (char.IsDigit(code[i]))
+                        {
+                            builder.Append(code[i]);
+                            current_string = builder.ToString();
+                            state = 2;
+                            i++;
+                        }
+                        else
+                        {
+                            values.Add(current_string);
+                            types.Add("number");
+                            current_string = "";
+                            builder.Clear();
+                            state = 0;
+                        }
+                        break;
+                    case 3://id
+                        if (char.IsLetterOrDigit(code[i]))
+                        {
+                            builder.Append(code[i]);
+                            current_string = builder.ToString();
+                            state = 3;
+                            i++;
+                        }
+                        else
+                        {
+                            values.Add(current_string);
+                            if(isReserved(current_string))
+                            {
+                                types.Add("reserved");
+                            }
+                            else
+                            {
+                                types.Add("identifier");
+                            }
+                            current_string = "";
+                            builder.Clear();
+                            state = 0;
+                        }
+                        break;
+                    case 4://assign
+                        if (code[i]=='=')
+                        {
+                            builder.Append(code[i]);
+                            current_string = builder.ToString();
+                            values.Add(current_string);
+                            types.Add("assign");
+                            current_string = "";
+                            builder.Clear();
+                            state = 0;
+                            i++;
+                        }
+                        
+
+                        break;
+                }
+            }
+        }
+
+
+
+    }
+}
